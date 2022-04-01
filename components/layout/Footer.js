4b33a423ba71dc03b5deeb6df0f8595982424224
pnam29 <phuongnamhn2000@gmail.com>
2022-04-01 18:04:24 +0700

    update footer

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -1,37 +1,57 @@
-import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 
-const Footer = () => (
-  <footer>
-    <div className="container">
-      <div className="left">
-        <p className="text-4xl text-white font-bold">Sẵn sàng tham gia dự án?</p>
-        <p className="text-white mt-5 mb-10">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.
-        </p>
-        <button type="button" className="button ">
-          Đăng ký
-        </button>
-      </div>
+import { Link, scrollSpy } from "react-scroll";
 
-      <div className="flex flex-row flex-wrap md:flex-col md:justify-between mt-4 md:mt-0">
-        <Link href={"#"} prefetch={false}>
-          <a className="text-white hover:text-pink-200 mr-2">Navigation</a>
-        </Link>
-        <Link href={"#"} prefetch={false}>
-          <a className="text-white hover:text-pink-200 mr-2">Email Marketing</a>
-        </Link>
-        <Link href={"#"} prefetch={false}>
-          <a className="text-white hover:text-pink-200 mr-2">Campaigns</a>
-        </Link>
-        <Link href={"#"} prefetch={false}>
-          <a className="text-white hover:text-pink-200 mr-2">Branding</a>
-        </Link>
-        <Link href={"#"} prefetch={false}>
-          <a className="text-white hover:text-pink-200 mr-2">Offline</a>
-        </Link>
+const list = [
+  { id: "home", label: "Trang chủ" },
+  { id: "about", label: "Về dự án" },
+  { id: "subjects", label: "Nội dung đào tạo" },
+  { id: "mentors", label: "Mentor" },
+  { id: "news", label: "Tin tức - Sự kiện" },
+  { id: "contact", label: "Liên hệ" },
+];
+
+const Footer = () => {
+  useEffect(() => {
+    scrollSpy.update();
+  }, []);
+
+  return (
+    <footer>
+      <div className="container">
+        <div className="left">
+          <h4 className="font-semibold">Sẵn sàng tham gia dự án?</h4>
+          <h6 className="text-white mt-5 mb-10">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt.
+          </h6>
+          <button type="button" className="btn button">
+            Đăng ký
+          </button>
+        </div>
+
+        <div className="right">
+          <h6>Điều hướng</h6>
+          <ul>
+            {list.map((item) => (
+              <li key={item.id}>
+                <Link
+                  activeClass="active"
+                  to={item.id}
+                  spy={true}
+                  smooth={true}
+                  offset={-80}
+                  duration={500}
+                  delay={200}
+                >
+                  {item.label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-    </div>
-  </footer>
-);
+    </footer>
+  );
+};
 export default Footer;
